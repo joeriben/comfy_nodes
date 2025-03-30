520d8f92e196eef5092f886225fbbb3b1c910ddc
joeriben <joerissen@gmail.com>
2025-03-31 00:03:57 +0200

    Update simple_text_display.py

--- simple_text_display.py
+++ simple_text_display.py
@@ -1,19 +1,17 @@
 class SimpleTextDisplay:
-    def __init__(self):
-        pass
-
     @classmethod
     def INPUT_TYPES(cls):
         return {
             "required": {
-                "text": ("STRING", {"multiline": True, "forceInput": True}),
+                "text": ("STRING", {"forceInput": True, "multiline": True}),
             }
         }
 
     RETURN_TYPES = ("STRING",)
     RETURN_NAMES = ("text",)
-    FUNCTION = "forward"
-    CATEGORY = "Utility"
+    FUNCTION = "display"
+    CATEGORY = "Utils"
 
-    def forward(self, text):
+    def display(self, text):
+        # Nur zurückgeben, damit es weitergeleitet werden kann
         return (text,)
