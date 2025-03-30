d38afcf5241192d962f232310cb285aa2ba9a9da
joeriben <joerissen@gmail.com>
2025-03-30 17:38:39 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
-from .ollama_prompt import OllamaPrompt
+from .ollama_prompt import ai4artsed_ollama
 
 NODE_CLASS_MAPPINGS = {
-    "OllamaPrompt": OllamaPrompt,
+    "ai4artsed_ollama": ai4artsed_ollama,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "OllamaPrompt": "Ollama Prompt",
+    "ai4artsed_ollama": "AI4ArtsEd Ollama Prompt",
 }
