08fae3867e9d0c190fcfbcc6f0d4ed2920ffe162
joeriben <joerissen@gmail.com>
2025-03-30 11:29:07 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .ollama_prompt import OllamaPromptNode
+
+NODE_CLASS_MAPPINGS = {
+    "OllamaPromptNode": OllamaPromptNode,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "OllamaPromptNode": "Ollama_Prompt",
+}
