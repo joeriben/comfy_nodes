e22a9a32eb9fa4737fdb0624fb4adaf809065592
joeriben <joerissen@gmail.com>
2025-03-30 23:23:42 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,12 @@
 from .ollama_prompt import ai4artsed_ollama
+from .simple_text_display import SimpleTextDisplay
 
 NODE_CLASS_MAPPINGS = {
     "ai4artsed_ollama": ai4artsed_ollama,
+    "SimpleTextDisplay": SimpleTextDisplay,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "ai4artsed_ollama": "AI4ArtsEd Ollama Prompt",
+    "SimpleTextDisplay": "🖹 Simple Text Display",
 }
