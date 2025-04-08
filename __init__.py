a3fb102eb36b14933e6433457a9c29327bb02cd2
joeriben <joerissen@gmail.com>
2025-04-08 16:07:33 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,15 @@
 from .ollama_prompt import ai4artsed_ollama
 from .simple_text_display import SimpleTextDisplay
+from .ai4artsed_openrouter import ai4artsed_openrouter
 
 NODE_CLASS_MAPPINGS = {
     "ai4artsed_ollama": ai4artsed_ollama,
     "SimpleTextDisplay": SimpleTextDisplay,
+    "ai4artsed_openrouter": ai4artsed_openrouter,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "ai4artsed_ollama": "AI4ArtsEd Ollama Prompt",
     "SimpleTextDisplay": "Simple Text Display",
+    "ai4artsed_openrouter": "AI4ArtsEd OpenRouter Prompt",
 }
