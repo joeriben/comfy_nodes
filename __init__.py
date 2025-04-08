026ac9295cf207117c107a3031fb137611ac2f64
joeriben <joerissen@gmail.com>
2025-04-08 21:01:20 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,15 @@
 from .ai4artsed_ollama import ai4artsed_ollama
 from .ai4artsed_openrouter import ai4artsed_openrouter
+from .ai4artsed_openrouter_imageanalysis import AI4ArtsEd_OpenRouter_ImageAnalysis
 
 NODE_CLASS_MAPPINGS = {
     "ai4artsed_ollama": ai4artsed_ollama,
     "ai4artsed_openrouter": ai4artsed_openrouter,
+    "ai4artsed_openrouter_imageanalysis": AI4ArtsEd_OpenRouter_ImageAnalysis,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "ai4artsed_ollama": "AI4ArtsEd Ollama Promptinterception",
     "ai4artsed_openrouter": "AI4ArtsEd OpenRouter Promptinterception",
+    "ai4artsed_openrouter_imageanalysis": "AI4ArtsEd OpenRouter Image Analysis",
 }
