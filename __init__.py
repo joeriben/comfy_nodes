74b935530fc5b753475bca496655549b9c977e63
joeriben <joerissen@gmail.com>
2025-04-10 20:37:33 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,8 +3,8 @@ from .ai4artsed_ollama_imageanalysis import ai4artsed_ollama_imageanalysis
 from .ai4artsed_openrouter import ai4artsed_openrouter
 from .ai4artsed_openrouter_imageanalysis import ai4artsed_openrouter_imageanalysis
 from .ai4artsed_text_remix import ai4artsed_text_remix
-from .ai4artsed_text_transform_creative import ai4artsed_text_transform_creative
-from .ai4artsed_text_transform_artform import ai4artsed_text_transform_artform
+from .ai4artsed_random_instruction_generator import ai4artsed_random_instruction_generator
+from .ai4artsed_random_artform_generator import ai4artsed_random_artform_generator
 
 NODE_CLASS_MAPPINGS = {
     "ai4artsed_ollama": ai4artsed_ollama,
@@ -12,8 +12,8 @@ NODE_CLASS_MAPPINGS = {
     "ai4artsed_openrouter": ai4artsed_openrouter,
     "ai4artsed_openrouter_imageanalysis": ai4artsed_openrouter_imageanalysis,
     "ai4artsed_text_remix": ai4artsed_text_remix,
-    "ai4artsed_text_transform_creative": ai4artsed_text_transform_creative,
-    "ai4artsed_text_transform_artform": ai4artsed_text_transform_artform,
+    "ai4artsed_random_instruction_generator": ai4artsed_random_instruction_generator,
+    "ai4artsed_random_artform_generator": ai4artsed_random_artform_generator,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -22,6 +22,6 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "ai4artsed_openrouter": "AI4ArtsEd OpenRouter Promptinterception",
     "ai4artsed_openrouter_imageanalysis": "AI4ArtsEd OpenRouter Image Analysis",
     "ai4artsed_text_remix": "AI4ArtsEd Text Remix",
-    "ai4artsed_text_transform_creative": "Text Transform Creative",
-    "ai4artsed_text_transform_artform": "Text Transform Artform",
+    "ai4artsed_random_instruction_generator": "Random Instruction Generator",
+    "ai4artsed_random_artform_generator": "Random Artform Generator",
 }
