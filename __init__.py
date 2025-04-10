8c83fc124a0b5d568d3b4b1e61e68ad810a4910c
joeriben <joerissen@gmail.com>
2025-04-10 20:01:29 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,6 +3,8 @@ from .ai4artsed_ollama_imageanalysis import ai4artsed_ollama_imageanalysis
 from .ai4artsed_openrouter import ai4artsed_openrouter
 from .ai4artsed_openrouter_imageanalysis import ai4artsed_openrouter_imageanalysis
 from .ai4artsed_text_remix import ai4artsed_text_remix
+from .ai4artsed_text_transform_creative import ai4artsed_text_transform_creative
+from .ai4artsed_text_transform_artform import ai4artsed_text_transform_artform
 
 NODE_CLASS_MAPPINGS = {
     "ai4artsed_ollama": ai4artsed_ollama,
@@ -10,6 +12,8 @@ NODE_CLASS_MAPPINGS = {
     "ai4artsed_openrouter": ai4artsed_openrouter,
     "ai4artsed_openrouter_imageanalysis": ai4artsed_openrouter_imageanalysis,
     "ai4artsed_text_remix": ai4artsed_text_remix,
+    "ai4artsed_text_transform_creative": ai4artsed_text_transform_creative,
+    "ai4artsed_text_transform_artform": ai4artsed_text_transform_artform,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -18,4 +22,6 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "ai4artsed_openrouter": "AI4ArtsEd OpenRouter Promptinterception",
     "ai4artsed_openrouter_imageanalysis": "AI4ArtsEd OpenRouter Image Analysis",
     "ai4artsed_text_remix": "AI4ArtsEd Text Remix",
+    "ai4artsed_text_transform_creative": "Text Transform Creative",
+    "ai4artsed_text_transform_artform": "Text Transform Artform",
 }
