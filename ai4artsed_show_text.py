9dfe89b08e1652359d15255764e9a5d9c803850f
joeriben <joerissen@gmail.com>
2025-04-12 00:49:01 +0200

    Update ai4artsed_show_text.py

--- ai4artsed_show_text.py
+++ ai4artsed_show_text.py
@@ -1,72 +1,19 @@
-# ai4artsed_show_text.py
-#
-# Adapted from:
-#   https://github.com/fairy-root/ComfyUI-Show-Text
-#   Original author: fairy-root
-#
-#MIT License
-#
-#Copyright (c) 2025 FairyRoot
-#
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-#
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
-#
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
-#
-
 class ai4artsed_show_text:
     @classmethod
     def INPUT_TYPES(cls):
         return {
             "required": {
                 "text": ("STRING", {"forceInput": True}),
-            },
-            "hidden": {
-                "unique_id": "UNIQUE_ID",
-                "extra_pnginfo": "EXTRA_PNGINFO",
-            },
+            }
         }
 
-    INPUT_IS_LIST = True
-    RETURN_TYPES = ("STRING",)
-    FUNCTION = "notify"
+    RETURN_TYPES = ()
+    FUNCTION = "show"
     OUTPUT_NODE = True
-    OUTPUT_IS_LIST = (True,)
-
     CATEGORY = "AI4ArtsEd"
 
-    def notify(self, text, unique_id=None, extra_pnginfo=None):
-        if unique_id is not None and extra_pnginfo is not None:
-            if not isinstance(extra_pnginfo, list):
-                print("Error: extra_pnginfo is not a list")
-            elif (
-                not isinstance(extra_pnginfo[0], dict)
-                or "workflow" not in extra_pnginfo[0]
-            ):
-                print("Error: extra_pnginfo[0] is not a dict or missing 'workflow' key")
-            else:
-                workflow = extra_pnginfo[0]["workflow"]
-                node = next(
-                    (x for x in workflow["nodes"] if str(x["id"]) == str(unique_id[0])),
-                    None,
-                )
-                if node:
-                    node["widgets_values"] = [text]
-
-        return {"ui": {"text": text}, "result": (text,)}
+    def show(self, text):
+        return {"ui": {"text": text}}
 
 
 NODE_CLASS_MAPPINGS = {
@@ -76,4 +23,3 @@ NODE_CLASS_MAPPINGS = {
 NODE_DISPLAY_NAME_MAPPINGS = {
     "ai4artsed_show_text": "Show Text",
 }
-
